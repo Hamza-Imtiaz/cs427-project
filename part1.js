53df68f7ed6947b5ce84a807f95c0dfd08014df3
Hamza-Imtiaz <63941147+Hamza-Imtiaz@users.noreply.github.com>
2020-04-19 23:18:04 -0600

    Add files via upload

---
+++ part1.js
@@ -0,0 +1,105 @@
+//Global Variables
+var angle = 0.0;
+var button, button2;
+var isPlaying = false;
+var song;
+var num = 0.10;
+var counter = 0;
+var Slider;
+var x = 0;
+
+//Preloading the audio file
+function preload() {
+  song = loadSound("Tone.mp3");
+}
+
+//Setting up the canvas/backgroud
+function setup() {
+  createCanvas(620, 400);
+  noStroke();
+  fill(100);
+  rectMode(CENTER);
+
+//Creating play and pause button
+  button = createButton('Play');
+  button.position(30, 140);
+  button.size(90, 30);
+  button.mousePressed(start);
+
+//Creating Volume slider
+  slider = createSlider(0, 100, 50);
+  slider.position(30, 180);
+}
+
+//Drawing the background and the elements needed for this project
+function draw() {
+  background(51);
+  translate(width / 2, height / 2);
+  button.html('Play');
+  angle += 0.30;
+  
+  //Writting volume by slider
+  textSize(15);
+  fill(255);
+  text('Volume', -236, -101);
+
+  //Calling function to change color when it's reach to certain point
+  fill(toggleColor());
+  
+  //Spinning the shape
+  rotate(angle);
+
+  //checking if it's playing
+  if (isPlaying == true) {
+    button.html('Pause');
+    rect(0, 0, 180, 180);
+
+    //Setting up the values of slider
+    let val = slider.value();
+    if (val > 0)
+      song.setVolume(0.25);
+    else if (val > 25)
+      song.setVolume(0.5);
+    else if (val > 50)
+      song.setVolume(0.75);
+    else if (val > 75)
+      song.setVolume(1.0);
+    else
+      song.setVolume(0.0);
+  }
+
+}
+
+//Function to change the shape's color
+function toggleColor() {
+  let col = color(0, 255, 0);
+  var delay = 500;
+  if (x < delay * 2 + 1)
+    x++;
+
+  //Statement where it checkes if its a time to change the color
+  if (x > 0 && x <= delay)
+    col = color(0, 255, 0);
+  else if (x > delay && x <= delay * 2)
+    col = color(255, 255, 15);
+  else if (x > delay * 2)
+    col = color(255, 0, 0);
+  return col;
+}
+
+//function to check if pressing the play button playing or not
+//Also, checking if the song if palying or not
+function start() {
+  if (isPlaying == true) {
+    isPlaying = false;
+  } else if (isPlaying == false) {
+    isPlaying = true;
+  }
+  
+//Checking for dong
+  if (!song.isPlaying()) {
+    song.play();
+  } else {
+    song.pause();
+  }
+}
---
+++ part2.js
@@ -0,0 +1,104 @@
+//Creating stings of piano notes
+var notes = [59, 60, 62, 64, 65, 67, 69, 71, 74];
+
+//setting up the page where i create the canvas and piano keys
+function setup() {
+    createCanvas(500, 400);
+
+    var div0 = createDiv("G5")
+    div0.position(16, 250);
+    var div1 = createDiv("G#5")
+    div1.position(66, 250);
+    var div2 = createDiv("A#5")
+    div2.position(123, 250);
+    var div3 = createDiv("C6")
+    div3.position(183, 250);
+    var div4 = createDiv("C#6")
+    div4.position(234, 250);
+    var div5 = createDiv("D#6")
+    div5.position(290, 250);
+    var div6 = createDiv("F6")
+    div6.position(350, 250);
+    var div7 = createDiv("G6")
+    div7.position(405, 250);
+    var div8 = createDiv("A#6")
+    div8.position(456, 250);
+
+    // Square oscillator
+    osc = new p5.Oscillator('square');
+
+    // Start the piano silently
+    osc.start();
+    osc.amp(0);
+
+    // Createing drop down menu for differnt sound effects
+    sel = createSelect();
+    sel.position(5, 115);
+    sel.option('square');
+    sel.option('triangle');
+    sel.option('sine');
+    sel.selected('square');
+    sel.changed(mySelectEvent);
+
+    //Fuction to play selected drop down menu option
+    function mySelectEvent() {
+        let val = sel.value();
+        if (val == 'square') {
+            bgCol = new p5.Oscillator('square');
+        } else if (val == 'triangle') {
+            bgCol = new p5.Oscillator('triangle');
+        } else if (val == 'sine') {
+            bgCol = new p5.Oscillator('sine');
+        }
+    }
+}
+
+//Function to play a piano notes
+function playNote(note, duration) {
+    osc.freq(midiToFreq(note));
+    // Fading the tone
+    osc.fade(0.5, 0.2);
+
+    //Fading out the tone
+    if (duration) {
+        setTimeout(function() {
+            osc.fade(0, 0.2);
+        }, duration - 50);
+    }
+}
+
+//Function to draw the piano 
+function draw() {
+    // Draw a keyboard
+    //Widht and leght of keys
+    //keys can be increase if user want to all they have to do is add new note to note string on top and additional key will appear.
+    var w = width / notes.length;
+    for (var i = 0; i < notes.length; i++) {
+        var x = i * w;
+        // Checking if the mouse is over the key
+        if (mouseX > x && mouseX < x + w && mouseY < height) {
+            // Checking if user is clicking anf if it is change the key color
+            if (mouseIsPressed) {
+                fill(36, 126, 161);
+                // Or if just hovering over use this color
+            } else {
+                fill(255, 215, 10);
+            } // otherwise leave it to drfault 
+        } else {
+            fill(26, 146, 15);
+        }
+        //Setting up the key width and height
+        rect(x, 30, w - 5, height - 198);
+    }
+}
+
+//Function for mouse pressed 
+function mousePressed() {
+    // Map mouse to the key index
+    var key = floor(map(mouseX, 0, width, 0, notes.length));
+    playNote(notes[key]);
+}
+// Fading out the tone when user release the mouse
+function mouseReleased() {
+    osc.fade(0, 0.5);
+}
